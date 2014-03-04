c7ffb1655e00a52d67e07f8650bbf26425090492
Thomas Welton <thomaswelton@me.com>
2014-03-04 22:11:36 +0000

    Added env var debug to run script, fixed variable interpolation issue

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@
 # Copy env vars to file for permanent storage
 env | grep _ >> /etc/environment
 
+echo -e "\nSaving ENV VARS...\n"
+cat /etc/environment
+
 # Copy the varnish backend to the config file
-echo 'backend default { .host = "$VARNISH_BACKEND_PORT_80_TCP_ADDR"; .port = "$VARNISH_BACKEND_PORT_80_TCP_PORT"; }' >> /etc/varnish/default.vcl
+echo "backend default { .host = \"$VARNISH_BACKEND_PORT_80_TCP_ADDR\"; .port = \"$VARNISH_BACKEND_PORT_80_TCP_PORT\"; }" >> /etc/varnish/default.vcl
 
 varnishd -f /etc/varnish/default.vcl  -a 0.0.0.0:80 -s malloc,1G -F
